65be6001681c05cee5fe8fc2e4748ef013a441f4
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-09-29 14:42:04 +0300

    update images loading problem

--- src/data.js
+++ src/data.js
@@ -1,8 +1,17 @@
+import phoneOne from './img/phoneOne.png';
+import phoneTwo from './img/phoneOne.png';
+import phoneThree from './img/phoneOne.png';
+import phoneFour from './img/phoneOne.png';
+import phoneFive from './img/phoneOne.png';
+import phoneSix from './img/phoneOne.png';
+import phoneSeven from './img/phoneOne.png';
+import phoneEight from './img/phoneOne.png';
+
 export const storeProducts = [
   {
     id: 1,
     title: "Google Pixel - Black",
-    img: "img/product-1.png",
+    img: phoneOne,
     price: 10,
     company: "GOOGLE",
     info:
@@ -14,7 +23,7 @@ export const storeProducts = [
   {
     id: 2,
     title: "Samsung S7",
-    img: "img/product-2.png",
+    img: phoneTwo,
     price: 16,
     company: "SAMSUNG",
     info:
@@ -26,7 +35,7 @@ export const storeProducts = [
   {
     id: 3,
     title: "HTC 10 - Black",
-    img: "img/product-3.png",
+    img: phoneThree,
     price: 8,
     company: "htc",
     info:
@@ -38,7 +47,7 @@ export const storeProducts = [
   {
     id: 4,
     title: "HTC 10 - White",
-    img: "img/product-4.png",
+    img: phoneFour,
     price: 18,
     company: "htc",
     info:
@@ -50,7 +59,7 @@ export const storeProducts = [
   {
     id: 5,
     title: "HTC Desire 626s",
-    img: "img/product-5.png",
+    img: phoneFive,
     price: 24,
     company: "htc",
     info:
@@ -62,7 +71,7 @@ export const storeProducts = [
   {
     id: 6,
     title: "Vintage Iphone",
-    img: "img/product-6.png",
+    img: phoneSix,
     price: 17,
     company: "apple",
     info:
@@ -74,7 +83,7 @@ export const storeProducts = [
   {
     id: 7,
     title: "Iphone 7",
-    img: "img/product-7.png",
+    img: phoneSeven,
     price: 30,
     company: "apple",
     info:
@@ -86,7 +95,7 @@ export const storeProducts = [
   {
     id: 8,
     title: "Smashed Iphone",
-    img: "img/product-8.png",
+    img: phoneEight,
     price: 2,
     company: "apple",
     info:
@@ -100,7 +109,7 @@ export const storeProducts = [
 export const detailProduct = {
   id: 1,
   title: "Google Pixel - Black",
-  img: "img/product-1.png",
+  img: phoneOne,
   price: 10,
   company: "google",
   info:
