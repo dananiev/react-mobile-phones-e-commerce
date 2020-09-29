24a733d6e4fee3c84920d14b21360f4a0b4cd5eb
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-09-29 13:52:18 +0300

    Updates

---
+++ precache-manifest.1698a89cdf2810eb278ffce31e8de5d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d984da3b1e8d2096540b54a4d80332d",
+    "url": "/react-mobile-phones-e-commerce/index.html"
+  },
+  {
+    "revision": "928565eb15ed88709f3b",
+    "url": "/react-mobile-phones-e-commerce/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1d65202a29074d672cf2",
+    "url": "/react-mobile-phones-e-commerce/static/css/main.ad18886e.chunk.css"
+  },
+  {
+    "revision": "928565eb15ed88709f3b",
+    "url": "/react-mobile-phones-e-commerce/static/js/2.fb4c2dd6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-mobile-phones-e-commerce/static/js/2.fb4c2dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d65202a29074d672cf2",
+    "url": "/react-mobile-phones-e-commerce/static/js/main.b4105152.chunk.js"
+  },
+  {
+    "revision": "7fb0a8e11ede3ac2f8e4",
+    "url": "/react-mobile-phones-e-commerce/static/js/runtime-main.38ad787f.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react-mobile-phones-e-commerce/static/media/logo.9f7e506d.svg"
+  }
+]);
